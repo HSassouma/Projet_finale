9b74b6cac3fbe5265decab66d0d8225da63a31ba
HSassouma <elhoussine.assouma.estn25@ump.ac.ma>
2026-09-18 17:22:49 +0200

    remplir data

--- src/Data.js
+++ src/Data.js
@@ -27,6 +27,123 @@ const apprenants = [
       { jour: 1, exercicesTermines: 14,
         totalExercices: 20, challengeTermine: true }
     ]
-  }
+  },
+  {
+  id: 4,
+  nomComplet: "Imane JavaScript",
+  ville: "Casablanca",
+  resultats: [
+    {
+      jour: 1,
+      exercicesTermines: 19,
+      totalExercices: 20,
+      challengeTermine: true
+    },
+    {
+      jour: 2,
+      exercicesTermines: 18,
+      totalExercices: 20,
+      challengeTermine: true
+    },
+    {
+      jour: 3,
+      exercicesTermines: 17,
+      totalExercices: 20,
+      challengeTermine: true
+    }
+  ]
+},
+
+{
+  id: 5,
+  nomComplet: "Omar Web",
+  ville: "Tanger",
+  resultats: [
+    {
+      jour: 1,
+      exercicesTermines: 10,
+      totalExercices: 20,
+      challengeTermine: false
+    },
+    {
+      jour: 2,
+      exercicesTermines: 12,
+      totalExercices: 20,
+      challengeTermine: true
+    }
+  ]
+},
+
+{
+  id: 6,
+  nomComplet: "Aya Dev",
+  ville: "Rabat",
+  resultats: [
+    {
+      jour: 1,
+      exercicesTermines: 8,
+      totalExercices: 20,
+      challengeTermine: false
+    },
+    {
+      jour: 2,
+      exercicesTermines: 9,
+      totalExercices: 20,
+      challengeTermine: false
+    },
+    {
+      jour: 3,
+      exercicesTermines: 7,
+      totalExercices: 20,
+      challengeTermine: true
+    }
+  ]
+},
+
+{
+  id: 7,
+  nomComplet: "Hamza Code",
+  ville: "Fès",
+  resultats: [
+    {
+      jour: 1,
+      exercicesTermines: 15,
+      totalExercices: 20,
+      challengeTermine: true
+    },
+    {
+      jour: 2,
+      exercicesTermines: 14,
+      totalExercices: 20,
+      challengeTermine: false
+    },
+    {
+      jour: 3,
+      exercicesTermines: 16,
+      totalExercices: 20,
+      challengeTermine: true
+    },
+    {
+      jour: 4,
+      exercicesTermines: 13,
+      totalExercices: 20,
+      challengeTermine: true
+    }
+  ]
+},
+
+{
+  id: 8,
+  nomComplet: "Nora FullStack",
+  ville: "Marrakech",
+  resultats: [
+    {
+      jour: 1,
+      exercicesTermines: 5,
+      totalExercices: 20,
+      challengeTermine: false
+    }
+  ]
+}
 ];
 module.exports = apprenants;
